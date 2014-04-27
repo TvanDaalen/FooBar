b5b13d444da045feaff86bb44b91a8af1c0f3895
anton <anton.schreck@student.hswt.de>
2014-04-27 13:31:30 +0200

    Add default comment

--- src/de/muc/tvd/foobar/FooBarMain.java
+++ src/de/muc/tvd/foobar/FooBarMain.java
@@ -3,6 +3,7 @@ package de.muc.tvd.foobar;
 public class FooBarMain {
 
 	public static void main(String[] args) {
+		// Default S.o.p
 		System.out.println("Hello World and welcome to FooBar!");
 	}
 
