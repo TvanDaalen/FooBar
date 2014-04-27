d70238f6355a3b482037eb96416ecaab008962d7
anton <anton.schreck@student.hswt.de>
2014-04-27 14:18:36 +0200

    Add testing comment for linux

--- src/de/muc/tvd/foobar/FooBarMain.java
+++ src/de/muc/tvd/foobar/FooBarMain.java
@@ -5,7 +5,8 @@ public class FooBarMain {
 	public static void main(String[] args) {
 		// Default S.o.p
 		System.out.println("Hello World and welcome to FooBar!");
-		// Comment for testing: Ä, ä, Ö, ö, Ü, ü, ß
+		// Comment for testing: ï¿½, ï¿½, ï¿½, ï¿½, ï¿½, ï¿½, ï¿½ <- windows
+		// kubuntu -> Ã„, Ã¤, Ã–, Ã¶, Ãœ, Ã¼, ÃŸ
 	}
 
 }
