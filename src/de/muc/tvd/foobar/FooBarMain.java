51bc112333fd94b35d0104beb6e13ac4bbaa5557
Anton <anton.schreck@web.de>
2014-04-27 14:08:28 +0200

    Add testing comment

--- src/de/muc/tvd/foobar/FooBarMain.java
+++ src/de/muc/tvd/foobar/FooBarMain.java
@@ -5,6 +5,7 @@ public class FooBarMain {
 	public static void main(String[] args) {
 		// Default S.o.p
 		System.out.println("Hello World and welcome to FooBar!");
+		// Comment for testing: Ä, ä, Ö, ö, Ü, ü, ß
 	}
 
 }
