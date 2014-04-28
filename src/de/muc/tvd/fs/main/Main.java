6e802eb59c2a07340c88123ef0ab7ae2bf40cd04
anton <anton.schreck@student.hswt.de>
2014-04-28 15:51:44 +0200

    Cast to Parent

--- src/de/muc/tvd/fs/main/Main.java
+++ src/de/muc/tvd/fs/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+		Parent root = (Parent) FXMLLoader.load(getClass().getResource("MainView.fxml"));
 		
 		Scene scene  = new Scene(root, 300, 200);
 		scene.getStylesheets().add(getClass().getResource("MainView.css").toExternalForm());
